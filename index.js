8eab60268b9936d7e6e169f0efe105033f5ddc05
unknown <urrutia_987@hotmail.com>
2023-10-25 15:50:25 -0600

    cambios deploy render

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ const corsOptions = {
     }
 }
 app.use(cors(corsOptions))
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        process.env.FRONTEND_URL
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    )
+});
 
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
